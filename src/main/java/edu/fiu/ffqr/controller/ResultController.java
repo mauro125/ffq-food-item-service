302e766628d197c84c61fdbb91e9f09d392d9f29
The D Code <thedcode86@gmail.com>
2019-09-21 16:14:36 -0400

    adding result controller

---
+++ src/main/java/edu/fiu/ffqr/controller/ResultController.java
@@ -0,0 +1,35 @@
+package edu.fiu.ffqr.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import edu.fiu.ffqr.models.Result;
+import edu.fiu.ffqr.service.ResultsService;
+
+/*
+ * Author: Dariana Gonzalez
+ * Created: 09/2019
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/results")
+public class ResultController {
+	
+	@Autowired
+	private ResultsService resultsService;
+	
+	public ResultController() {}
+
+	@GetMapping("/all")
+	public List<Result> getAllResults() throws JsonProcessingException {
+		List<Result> results = resultsService.getAll();
+		return results;
+	}
+}
