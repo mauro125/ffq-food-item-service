fbf66dffedc8d0aa195c1a53e2d7c9585015ebe2
The D Code <thedcode86@gmail.com>
2019-11-09 22:53:19 -0500

    Added FoodRecommendationController (create and calculate endpoints)

---
+++ src/main/java/edu/fiu/ffqr/controller/FoodRecommendationController.java
@@ -0,0 +1,204 @@
+package edu.fiu.ffqr.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import edu.fiu.ffqr.models.FoodCategoryRecommendation;
+import edu.fiu.ffqr.models.FoodItem;
+import edu.fiu.ffqr.models.FoodItemInput;
+import edu.fiu.ffqr.models.FoodItemRecommendation;
+import edu.fiu.ffqr.models.FoodItemRecommendationRange;
+import edu.fiu.ffqr.models.FoodType;
+import edu.fiu.ffqr.models.Result;
+
+import edu.fiu.ffqr.models.SysFoodRecommendation;
+import edu.fiu.ffqr.service.FFQFoodEntryService;
+import edu.fiu.ffqr.service.ResultsService;
+import edu.fiu.ffqr.service.SysFoodItemRecommendationService;
+
+/*
+ * Author: Dariana Gonzalez
+ * Created: 11/2019
+ */
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/foodrecommendations")
+public class FoodRecommendationController {
+
+	@Autowired
+	private SysFoodItemRecommendationService SysFoodItemRecomService;
+	
+	@Autowired
+	private FFQFoodEntryService foodItemService;
+	
+	@Autowired
+	private ResultsService resultsService;
+	
+	public FoodRecommendationController() {}
+	
+	@PostMapping("/create")
+	public SysFoodRecommendation createSysFoodRecommendation(@RequestBody SysFoodRecommendation sysFoodRec) throws JsonProcessingException {
+		
+		if(SysFoodItemRecomService.getSysNutrientRecommendationByNutrientName(sysFoodRec.getCategoryName()) != null)
+		{
+			throw new IllegalArgumentException("System Nutrient Recommendation for nutrient " + sysFoodRec.getCategoryName() + " already exists.");			
+		}
+		else
+			return SysFoodItemRecomService.create(sysFoodRec);
+	}
+	
+	@GetMapping("/calculate/{questionnaireID}")
+	  public FoodItemRecommendation calculateFoodRecommendations(@PathVariable("questionnaireID") String questionnaireID) throws Exception {
+		
+		String foodItemName = "";
+		String nutrientListID = "";
+		String category = "";
+		int infantAge = 0;
+		String ageRange = "";
+		Double calculatedAmount = 0.0;		
+		Map<String, Double> categoryValueMap = new HashMap<String, Double>();
+		
+		// get results for given questionnaire
+		Result result = resultsService.getResultByQuestionnaireID(questionnaireID);
+		
+		infantAge = result.getAgeInMonths();
+		ArrayList<FoodItemInput> userChoices = result.getUserChoices();
+		
+		// instantiate Food Item Recommendation object
+		FoodItemRecommendation foodItemRecommendation = new FoodItemRecommendation();
+		
+		foodItemRecommendation.setQuestionnaireId(questionnaireID);
+		foodItemRecommendation.setPatientAgeInMonths(infantAge);
+		foodItemRecommendation.setPatientName("pending"); // patient name still no defined in the application
+			
+		// get list of food items recommendations by age
+		List<SysFoodRecommendation> SysFoodItemRecommendations = SysFoodItemRecomService.getAll();
+		
+		// populate the category-value map with the food item categories
+		for (SysFoodRecommendation sysFoodItemRecommendation: SysFoodItemRecommendations) {
+			categoryValueMap.put(sysFoodItemRecommendation.getCategoryName(), 0.0);
+		}
+		
+		// iterate list of user choices for food items
+		for (FoodItemInput foodItem: userChoices) {
+		
+			foodItemName = foodItem.getName();
+			nutrientListID = foodItem.getNutrientListID();
+			
+			// find the food item
+			FoodItem f = foodItemService.getEntryWithName(foodItemName);
+			
+			// find the food item type category
+			for (FoodType foodType: f.getFoodTypes()) {
+				if (foodType.getNutrientListID().equalsIgnoreCase(nutrientListID)) {
+					category = foodType.getCategory();
+				}
+			}
+			
+			// calculate total amount for food items categories
+			for (SysFoodRecommendation sysFoodItemRecommendation: SysFoodItemRecommendations) {
+				
+				String categoryName = sysFoodItemRecommendation.getCategoryName();
+				
+				if(category.equalsIgnoreCase(categoryName))
+				{
+					double currentTotal = 0.0;
+					
+					System.out.println(categoryValueMap.get(categoryName));
+					
+					if(foodItem.getServing() != null)
+						categoryValueMap.replace(categoryName, categoryValueMap.get(categoryName) + (Double.parseDouble(foodItem.getServing().split(" ")[0]) * foodItem.getFrequency()));
+					else
+						if(nutrientListID.equalsIgnoreCase("chee")) {
+														
+							currentTotal = (25.2 * foodItem.getFrequency() / 28.35);
+							
+							if (foodItem.getFrequencyType().equalsIgnoreCase("Week")) {
+								currentTotal = currentTotal / 7;
+							}
+							
+							categoryValueMap.replace(categoryName, categoryValueMap.get(categoryName) + currentTotal);
+						}
+						if(nutrientListID.equalsIgnoreCase("yogu")) {
+							
+							currentTotal = (113.4 * foodItem.getFrequency() / 28.35);
+							
+							if (foodItem.getFrequencyType().equalsIgnoreCase("Week")) {
+								currentTotal = currentTotal / 7;
+							}
+							
+							categoryValueMap.replace(categoryName, categoryValueMap.get(categoryName) + currentTotal);
+						}
+						if(nutrientListID.equalsIgnoreCase("soyp")) {
+														
+							currentTotal = (28.4 * foodItem.getFrequency() / 28.35);
+							
+							if (foodItem.getFrequencyType().equalsIgnoreCase("Week")) {
+								currentTotal = currentTotal / 7;
+							}
+							
+							categoryValueMap.replace(categoryName, categoryValueMap.get(categoryName) + currentTotal);
+						}
+				}
+			}
+		}
+		
+		for (SysFoodRecommendation sysFoodItemRecommendation: SysFoodItemRecommendations) {
+			
+			FoodCategoryRecommendation foodItemRec = new FoodCategoryRecommendation();
+			foodItemRec.setCategoryName(sysFoodItemRecommendation.getCategoryName());
+			foodItemRec.setLabel("");
+			calculatedAmount = categoryValueMap.get(sysFoodItemRecommendation.getCategoryName());
+			foodItemRec.setCalculatedAmount(calculatedAmount);
+			
+			if(infantAge >= 0 && infantAge <= 6)
+			{
+				ageRange = "0-6";
+			}
+			else if(infantAge >= 7 && infantAge <= 12)
+			{
+				ageRange = "7-12";
+			}
+			else if(infantAge >= 13 && infantAge <= 24)
+			{
+				ageRange = "13-24";
+			}
+			else
+				throw new Exception("There are no recommendations available for infants of age over 24 months");
+			
+			List<FoodItemRecommendationRange> rangeList = sysFoodItemRecommendation.getRecommendationsByAge().get(ageRange);
+			
+			boolean notFound = true;
+			
+			
+				for (FoodItemRecommendationRange range: rangeList) {
+					if (calculatedAmount >= range.getFrom() && calculatedAmount <= range.getTo() && notFound)
+					{
+						foodItemRec.setRangeFrom(range.getFrom());
+						foodItemRec.setRangeTo(range.getTo());
+						foodItemRec.setLabel(range.getLabel());
+						notFound = false;
+					}
+				}				
+			
+			
+			foodItemRecommendation.getFoodCategoryRecList().add(foodItemRec);
+		}
+		
+				
+		return foodItemRecommendation;
+	}
+}
