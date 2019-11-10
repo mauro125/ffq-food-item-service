01af4012c7f2e5d8bfeed647e6e20bf9213cdb28
The D Code <thedcode86@gmail.com>
2019-11-09 22:55:01 -0500

    Load Food Recommendation on service start

--- src/main/java/edu/fiu/ffqr/FFQFoodEntryApplication.java
+++ src/main/java/edu/fiu/ffqr/FFQFoodEntryApplication.java
@@ -37,6 +37,10 @@ public class FFQFoodEntryApplication {
 		// Added by Dariana Gonzalez 10/25/2019
 		// Load list of system nutrients recommendations
 		loader.loadSysNutrientsRecommendations();
+		
+		// Added by Dariana Gonzalez 10/25/2019
+	    // Load list of system food items recommendations
+		loader.loadSysFoodItemsRecommendations();
 	}
 	
 	@Bean
