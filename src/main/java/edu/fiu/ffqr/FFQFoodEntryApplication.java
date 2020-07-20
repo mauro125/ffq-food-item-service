007ded9a6d85ef376df6948145f59576db36d900
Darien Cot <dcot@univision.net>
2020-07-20 09:08:06 -0400

    Do not load data by default

--- src/main/java/edu/fiu/ffqr/FFQFoodEntryApplication.java
+++ src/main/java/edu/fiu/ffqr/FFQFoodEntryApplication.java
@@ -30,19 +30,19 @@ public class FFQFoodEntryApplication {
 		if (loadDataArg.equalsIgnoreCase("true")) {
 			DataLoader loader = (DataLoader)ctx.getBean(DataLoader.class);
 			loader.load();
+			
+			// Added by Dariana Gonzalez 10/25/2019
+			// Load list of system nutrients recommendations
+			loader.loadSysNutrientsRecommendations();
+			
+			// Added by Dariana Gonzalez 10/25/2019
+			// Load list of system food items recommendations
+			loader.loadSysFoodItemsRecommendations();
+
+			loader.loadFoodDescription();
 		}
 		
 		DataLoader loader = (DataLoader)ctx.getBean(DataLoader.class);
-		
-		// Added by Dariana Gonzalez 10/25/2019
-		// Load list of system nutrients recommendations
-		loader.loadSysNutrientsRecommendations();
-		
-		// Added by Dariana Gonzalez 10/25/2019
-	    // Load list of system food items recommendations
-		loader.loadSysFoodItemsRecommendations();
-
-		loader.loadFoodDescription();
 	}
 	
 	@Bean
